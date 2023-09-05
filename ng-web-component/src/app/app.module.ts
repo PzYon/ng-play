ea4cf377e964d970176f6bbc49a13753d0ce3e15
Markus Doggweiler <markus.doggweiler@gmail.com>
2023-09-05 23:05:22 +0200

    Initial commit (WIP)

---
+++ ng-web-component/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import {CUSTOM_ELEMENTS_SCHEMA, DoBootstrap, Injector, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {createCustomElement} from '@angular/elements';
+import {WebComponent} from './web.component';
+
+@NgModule({
+  declarations: [
+    WebComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [
+    WebComponent
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
+})
+export class AppModule implements DoBootstrap {
+  constructor(injector: Injector) {
+    if (customElements.get(WebComponent.tag)) {
+      return;
+    }
+
+    customElements.define(
+      WebComponent.tag,
+      createCustomElement(WebComponent, {
+        injector,
+      }),
+    );
+  }
+
+  ngDoBootstrap(): void {
+    // required to prevent https://angular.io/errors/NG0403
+  }
+}
---
+++ ng-web-component/src/app/web.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'web-component',
+  templateUrl: './web.component.html',
+  styleUrls: ['./web.component.scss']
+})
+export class WebComponent {
+  static tag = "web-component";
+}
